467c3671a9b46c598615802278f27752f8e5be1b
hmatejx <matej.horvat@gmail.com>
2017-05-06 13:05:18 +0200

    Improved noise model to more accurately reflect the Poissonian nature
    - added example fit to Facebook data
    - fixed a small bug where dates were erroneously treated as equidistant

--- facebook.R
+++ facebook.R
@@ -19,7 +19,7 @@ dd$Facebook <- 100 * dd$Facebook / max(dd$Facebook, na.rm = T)
 # prepare list to serve as data for STAN
 idx <- which(dd$Facebook > 0)
 fit_data <- list(T = length(idx),
-                 Time = seq(1, length(idx)),
+                 Time = as.numeric(dd$Date[idx] - dd$Date[idx[1] - 1])/30,
                  Y = dd$Facebook[idx])
 Y_meas = fit_data$Y
 
@@ -32,30 +32,35 @@ nChains <- 4
 fit <- stan(file = "irSIR.stan",
             data = fit_data,
             iter = iter,
-            init = rep(list(list(sigma = 0.07, beta = 0.15, nu = 0.05, N = 120, x = c(0.94, 0.01, 0.05))), nChains),
+            init = rep(list(list(sigma = 0.35, beta = 0.15, nu = 0.05, N = 120, x = c(0.94, 0.01, 0.05))), nChains),
             warmup = warmup,
             chains = nChains,
             cores = min(nChains, parallel::detectCores()),
             verbose = T)
 
 # print results
-print(fit, pars = c("sigma", "beta", "nu", "S0", "I0", "R0"))
+print(fit, pars = c("sigma", "beta", "nu", "N", "S0", "I0", "R0"))
 
 # explore the fit in Shiny
 # sso <- as.shinystan(fit)
 # launch_shinystan(sso)
 
 # posterior predictive check
-Y_pred <- t(apply(extract(fit, "Y_pred", permuted = F), 3, quantile, prob = c(0.05, 0.5, 0.95)))
+Y_pred <- t(apply(extract(fit, "Y_pred", permuted = F), 3, quantile, prob = c(0.025, 0.5, 0.975)))
 N_pred <- nrow(Y_pred)
-x <- c(dd$Date[idx], dd$Date[idx[length(idx)]] + 1:length(idx)*30)
-plot(x, rep(NA, 2*length(idx)), ylim = c(0, max(Y_pred)),
+Time_pred <- dd$Date[idx[1]] + (1:N_pred)*30
+plot(Time_pred, rep(NA, 2*length(idx)), ylim = c(0, max(Y_pred)),
      xlab = "Date", ylab = "Normalized popularity", 
      main = "Facebook popularity modeled by irSIR")
-polygon(c(x, rev(x)), c(Y_pred[, 1], rev(Y_pred[, 3])), col = "gray", border = NA)
-lines(x, Y_pred[, 2], lwd = 3)
+polygon(c(Time_pred, rev(Time_pred)), c(Y_pred[, 1], rev(Y_pred[, 3])),
+        col = "gray", border = NA)
+lines(Time_pred, Y_pred[, 2], lwd = 3)
 points(dd$Date[idx], Y_meas, pch = 21, bg = "white", cex = 1.2)
-abline(v = as.Date("2017-05-05"), col = "darkgray")
-legend("topright", bty = "n", legend = c("Line of best fit", "90% pred. int.", "Today"),
-       lty = c(1, NA, 1), lwd = c(3, NA, 1), pch = c(NA, 15, NA),
-       col = c("black", "gray", "darkgray"), pt.cex = c(NA, 2, NA))
+abline(v = as.Date("2017-05-05"), lty = 2)
+legend("topright", bty = "n", 
+       legend = c("Google trends data",
+                  "Line of best fit",
+                  "95% prediction interval",
+                  "Today"),
+       lty = c(NA, 1, NA, 2), lwd = c(NA, 3, NA, 1), pch = c(21, NA, 15, NA),
+       col = c("black", "black", "gray", "black"), pt.cex = c(1.2, NA, 2, NA))
